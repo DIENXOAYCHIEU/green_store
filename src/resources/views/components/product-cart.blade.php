287761f65e0e3deec1f55d132488115e49d0eeae
phuc <hongphuctg405@gmail.com>
2026-03-19 00:41:48 +0700

    responsive product, login pages

--- src/resources/views/components/product-cart.blade.php
+++ src/resources/views/components/product-cart.blade.php
@@ -1,9 +1,9 @@
 <dialog id='cart-popup' class="overflow-auto fixed z-5 w-full h-full inset-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" >		
 	{{-- close button --}}
 	<div class=" flex flex-row border-b-1 border-gray-300 p-4 pl-[2rem] justify-between items-center">
-		<p class="text-[1.3rem] font-bold">Giỏ hàng của bạn</p>
+		<p class="md:text-[1.3rem] font-bold">Giỏ hàng của bạn</p>
 		<button id="cart-close" class="flex items-center justify-center cursor-pointer">
-			<i class='text-[3rem] bx bx-x'></i>
+			<i class='md:text-[3rem] bx bx-x'></i>
 		</button>
 	</div>
 
@@ -11,7 +11,7 @@
 		@csrf
 		<input type="hidden" name="cart-input" id='cart-input'>
 		{{-- list products in cart --}}
-		<div id='cart-container' class="flex justify-center items-center pt-[2rem] pb-[2rem] ">
+		<div id='cart-container' class="flex justify-start min-w-[720px] overflow-x-auto md:justify-center items-center pt-[2rem] pb-[2rem] ">
 		</div>
 		<div id="checkout-btn" class="flex justify-center items-center p-4">
 		@if(Auth::check())
